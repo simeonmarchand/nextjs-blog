c3e70eaee9b723b76b01a33a100d3ea8701e53d9
Simeon Marchand <simeonrmarchand@gmail.com>
2023-04-18 14:16:01 -0500

    added FB script to first-post.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,13 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function firstPost(){
     return (
     <>
+      <Head>
+        <title>First Post</title>
+        <script src='https://connect.facebook.net/en_US/sdk.js'></script>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
