d01f789ce535f13a31d0aec00bee3df45d00bbe6
Simeon Marchand <simeonrmarchand@gmail.com>
2023-04-18 14:34:20 -0500

    added style to first-post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,18 +1,17 @@
-import Link from 'next/link';
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout from '../../components/layout';
 
-export default function firstPost(){
-    return (
-    <>
+export default function FirstPost() {
+  return (
+    <Layout>
       <Head>
         <title>First Post</title>
-        </Head>
-
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">← Back to home</Link>
       </h2>
-    </>
-    );
+    </Layout>
+  );
 }
