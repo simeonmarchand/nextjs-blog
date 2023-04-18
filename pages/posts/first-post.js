504e30bc3feebdce3163eec5ece8f240ed28f3de
Simeon Marchand <simeonrmarchand@gmail.com>
2023-04-18 14:22:54 -0500

    fixed script

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,16 +1,24 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function firstPost(){
     return (
     <>
       <Head>
         <title>First Post</title>
-        <script src='https://connect.facebook.net/en_US/sdk.js'></script>
-      </Head>
+        </Head>
+
+        <Script 
+          src='https://connect.facebook.net/en_US/sdk.js'
+          strategy='lazyOnload'
+          onLoad={() =>
+            console.log('script loaded correctly, window.FB has been populated')
+        }
+      />
       <h1>First Post</h1>
       <h2>
-        <Link href="/">Back to home</Link>
+        <Link href="/">← Back to home</Link>
       </h2>
     </>
     );
